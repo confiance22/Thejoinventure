67d4eb4130b1b3bc094d0cfb19f1499d7cb34285
Confiance UFITAMAHORO <confianceufitamahoro22@gmail.com>
2025-04-09 18:02:42 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,15 @@
 
+----Compare Values with Previous or Next Records (Salary)-----
+
+SELECT 
+    e.emp_id, e.name, e.salary, e.hire_date,
+    LAG(e.salary) OVER (ORDER BY e.hire_date) AS prev_salary,
+    LEAD(e.salary) OVER (ORDER BY e.hire_date) AS next_salary,
+    CASE 
+        WHEN e.salary > LAG(e.salary) OVER (ORDER BY e.hire_date) THEN 'HIGHER'
+        WHEN e.salary < LAG(e.salary) OVER (ORDER BY e.hire_date) THEN 'LOWER'
+        ELSE 'EQUAL'
+    END AS comparison
+FROM employees e;
+
+
